b6e0799645129b32d5b468455721d3c988a1e5ab
yadhu2309 <108937506+yadhu2309@users.noreply.github.com>
2023-06-28 11:59:53 +0530

    sss

--- epic_kart/settings.py
+++ epic_kart/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'epic_kart.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'epickart_notepromax',
+        'NAME': 'ekdb',
         'USER': 'postgres',
         'PASSWORD': 'admin',
         'HOST': 'localhost',
